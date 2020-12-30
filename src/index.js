0d9aec971111ba030f479ab6f2a695ef0940a495
tiannapalumbo <74277795+tiannapalumbo@users.noreply.github.com>
2020-12-30 16:47:00 -0500

    added wind and humidity

--- src/index.js
+++ src/index.js
@@ -31,9 +31,13 @@ function showTemp(response) {
   let city = document.querySelector("#name");
   let degrees = document.querySelector("#temperaturemain");
   let description = document.querySelector("#weatherdesc");
+  let wind = document.querySelector("#wind");
+  let humidity = document.querySelector("#humidity");
   let temperature = Math.round(response.data.main.temp);
   degrees.innerHTML = `${temperature}°`;
   description.innerHTML = `${response.data.weather[0].description}`;
+  wind.innerHTML = Math.round(response.data.main.wind.speed);
+  humidity.innerHTML = `${response.data.main.humidity}`;
   city.innerHTML = `${response.data.name}`;
 }
 function handleSubmit(event) {
