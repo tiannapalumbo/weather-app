80c9a239c0e9386fb615741f452c27d23e372467
tiannapalumbo <74277795+tiannapalumbo@users.noreply.github.com>
2021-01-01 20:09:24 -0500

    fix weather forecast

--- src/index.js
+++ src/index.js
@@ -65,11 +65,7 @@ function dispalyForecast(response) {
       <h3>
         ${formatHours(forecast.dt * 1000)}
       </h3>
-      <img
-        src="http://openweathermap.org/img/wn/${
-          forecast.weather[0].icon
-        }@2x.png"
-      />
+      <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
       <div class="weather-forecast-temperature">
         <strong>
           ${Math.round(forecast.main.temp_max)}°
