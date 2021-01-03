cde1092757627b81ed35389b9bdab21e6108fb4d
tiannapalumbo <74277795+tiannapalumbo@users.noreply.github.com>
2021-01-02 21:47:04 -0500

    made js link secure

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
       <h3>
         ${formatHours(forecast.dt * 1000)}
       </h3>
-      <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
+      <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
       <div class="weather-forecast-temperature">
         <strong>
           ${Math.round(forecast.main.temp_max)}°
